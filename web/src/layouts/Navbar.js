e1ad752565a72b86b0648ad111d479acd3c8e287
JustSxm <aypineb@gmail.com>
2022-09-23 20:37:46 -0400

    start website

--- web/src/layouts/Navbar.js
+++ web/src/layouts/Navbar.js
@@ -1,5 +1,29 @@
+import { faCircleInfo, faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import smartplant from "../images/smartplant.png";
+
 function Navbar() {
-	return <nav className=""></nav>;
+	return (
+		<nav className="absolute left-0 top-0 h-screen w-40">
+			<div className="flex flex-col h-screen center items-center">
+				<img
+					src={smartplant}
+					alt="logo"
+					width={60}
+					className="rounded-2xl mx-auto flex-grow-0 flex-shrink-1 mt-16"
+				></img>
+				<div className="flex-auto flex flex-col items-center justify-center">
+					<div className="bg-green m-5 p-5 w-16 h-16 flex flex-col items-center justify-center rounded-2xl text-white shadow-md">
+						<FontAwesomeIcon icon={faHouse} size="lg" />
+					</div>
+					<div className="m-5 p-5 text-gray-300">
+						<FontAwesomeIcon icon={faCircleInfo} />
+					</div>
+				</div>
+				<div className="flex-grow-0 flex-shrink-1 mb-10 h-[60px]"></div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
+		<div className="ml-40 mt-3 w-auto bg-secondgray/50 mb-3 mr-3 rounded-3xl p-12" id="test">
+			<h1 className="font-reem text-4xl font-bold tracking-wide">Overview</h1>
 		</div>
 	);
 }
