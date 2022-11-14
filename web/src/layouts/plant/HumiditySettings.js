d71f8377feb745b5623bd44578bf639d36626571
JustSxm <aypineb@gmail.com>
2022-11-14 11:43:12 -0500

    dashboard

--- web/src/layouts/plant/HumiditySettings.js
+++ web/src/layouts/plant/HumiditySettings.js
@@ -5,31 +5,19 @@ function HumiditySettings({ oldSettings, handleEditSettings }) {
 	return (
 		<div className="mt-5 px-7 py-5 bg-white rounded-3xl drop-shadow-2xl border-[1px] border-gray-100 hover:scale-110 duration-500 ease-in-out transition-transform">
 			<h2 className="text-2xl">
-				<FontAwesomeIcon
-					icon={faGear}
-					className="mr-2 text-blue-500"
-					size="sm"
-				/>
+				<FontAwesomeIcon icon={faGear} className="mr-2 text-blue-500" size="sm" />
 				Watering Threshold
 			</h2>
 			<div className="w-64 h-52 mt-2">
-				<label
-					for="toggleB"
-					class="flex items-center cursor-pointer select-none"
-				>
-					<div class="relative">
+				<label for="toggleB" class="flex items-center cursor-pointer select-none mb-2">
+					Enabled
+					<div class="ml-2 relative">
 						<input
 							type="checkbox"
 							id="toggleB"
 							class="sr-only"
 							checked={oldSettings.humidity.enabled}
-							onChange={(e) =>
-								handleEditSettings(
-									"humidity",
-									"enabled",
-									e.target.checked ? 1 : 0
-								)
-							}
+							onChange={(e) => handleEditSettings("humidity", "enabled", e.target.checked ? 1 : 0)}
 						/>
 						<div class="block bg-gray-600 w-14 h-6 rounded-full"></div>
 						<div class="dot absolute left-1 top-1 bg-red-400 w-4 h-4 rounded-full transition"></div>
@@ -38,18 +26,21 @@ function HumiditySettings({ oldSettings, handleEditSettings }) {
 						{oldSettings.humidity.enabled ? "On" : "Off"}
 					</div>
 				</label>
-				<input
-					type="numeric"
-					className="bg-gray-400"
-					defaultValue={oldSettings.humidity.humidity}
-					onChange={(e) =>
-						handleEditSettings(
-							"humidity",
-							"humidity",
-							Number(e.target.value)
-						)
-					}
-				/>
+				<div className="w-full">
+					<label for="numberPErcentage" className="inline-block">
+						Percentage
+					</label>
+					<input
+						type="number"
+						className=" rounded-full border-[1px] border-gray-400 px-3 inline-block w-auto"
+						defaultValue={oldSettings.humidity.humidity}
+						id="numberPErcentage"
+						onChange={(e) => handleEditSettings("humidity", "humidity", Number(e.target.value))}
+					/>
+					<h2 className="mt-2 text-xs text-gray-400">
+						The plant will be watered when the humidity is below this percentage.
+					</h2>
+				</div>
 			</div>
 		</div>
 	);
--- web/src/layouts/plant/IntervalSettings.js
+++ web/src/layouts/plant/IntervalSettings.js
@@ -5,31 +5,19 @@ function IntervalSettings({ oldSettings, handleEditSettings }) {
 	return (
 		<div className="mt-5 px-7 py-5 bg-white rounded-3xl drop-shadow-2xl border-[1px] border-gray-100 hover:scale-110 duration-500 ease-in-out transition-transform">
 			<h2 className="text-2xl">
-				<FontAwesomeIcon
-					icon={faGear}
-					className="mr-2 text-blue-500"
-					size="sm"
-				/>
+				<FontAwesomeIcon icon={faGear} className="mr-2 text-blue-500" size="sm" />
 				Watering Interval
 			</h2>
-			<div className="w-64 h-52 mt-2">
-				<label
-					for="toggleC"
-					class="flex items-center cursor-pointer select-none"
-				>
-					<div class="relative">
+			<div className="w-64 h-54 mt-2">
+				<label for="toggleC" class="flex items-center cursor-pointer select-none">
+					Enabled
+					<div className="relative ml-2">
 						<input
 							type="checkbox"
 							id="toggleC"
-							class="sr-only"
+							className="sr-only "
 							checked={oldSettings.interval.enabled}
-							onChange={(e) =>
-								handleEditSettings(
-									"interval",
-									"enabled",
-									e.target.checked ? 1 : 0
-								)
-							}
+							onChange={(e) => handleEditSettings("interval", "enabled", e.target.checked ? 1 : 0)}
 						/>
 						<div class="block bg-gray-600 w-14 h-6 rounded-full"></div>
 						<div class="dot absolute left-1 top-1 bg-red-400 w-4 h-4 rounded-full transition"></div>
@@ -38,44 +26,34 @@ function IntervalSettings({ oldSettings, handleEditSettings }) {
 						{oldSettings.interval.enabled ? "On" : "Off"}
 					</div>
 				</label>
-
+				<label className="block mt-1">Start Date</label>
 				<input
 					type="date"
-					defaultValue={
-						new Date(oldSettings.interval.firstWatering)
-							.toISOString()
-							.split("T")[0]
-					}
+					className="rounded-full border-[1px] border-gray-400 px-3"
+					defaultValue={new Date(oldSettings.interval.firstWatering).toISOString().split("T")[0]}
 					onChange={(e) => {
-						handleEditSettings(
-							"interval",
-							"firstWatering",
-							new Date(e.target.value).getTime()
-						);
+						handleEditSettings("interval", "firstWatering", new Date(e.target.value).getTime());
 					}}
 				/>
+				<h2 className="mt-1 text-xs text-gray-400">The date to use for the values below</h2>
+
+				<label className="block mt-1">Interval</label>
 				<input
-					type="numeric"
+					type="number"
 					defaultValue={oldSettings.interval.interval}
-					onChange={(e) =>
-						handleEditSettings(
-							"interval",
-							"interval",
-							Number(e.target.value)
-						)
-					}
+					className="rounded-full border-[1px] border-gray-400 px-3"
+					onChange={(e) => handleEditSettings("interval", "interval", Number(e.target.value))}
 				/>
+				<h2 className="mt-1 text-xs text-gray-400">Every x time to water the plant</h2>
+
+				<label className="block mt-1">Time to water</label>
 				<input
-					type="numeric"
+					type="number"
+					className="rounded-full border-[1px] border-gray-400 px-3"
 					defaultValue={oldSettings.interval.length}
-					onChange={(e) =>
-						handleEditSettings(
-							"interval",
-							"length",
-							Number(e.target.value)
-						)
-					}
+					onChange={(e) => handleEditSettings("interval", "length", Number(e.target.value))}
 				/>
+				<h2 className="mt-1 text-xs text-gray-400">How long to water the olant</h2>
 			</div>
 		</div>
 	);
