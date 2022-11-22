1fc8abace7db384643dd6b5aa3e40d18140836bb
pierreogagnon <pierre.olivier@kupastation.com>
2022-11-22 09:56:51 -0500

    Add dropdown in settings

--- web/src/layouts/plant/HumiditySettings.js
+++ web/src/layouts/plant/HumiditySettings.js
@@ -5,29 +5,42 @@ function HumiditySettings({ oldSettings, handleEditSettings }) {
 	return (
 		<div className="mt-5 px-7 py-5 bg-white rounded-3xl drop-shadow-2xl border-[1px] border-gray-100 hover:scale-110 duration-500 ease-in-out transition-transform">
 			<h2 className="text-2xl">
-				<FontAwesomeIcon icon={faGear} className="mr-2 text-blue-500" size="sm" />
+				<FontAwesomeIcon
+					icon={faGear}
+					className="mr-2 text-blue-500"
+					size="sm"
+				/>
 				Watering Threshold
 			</h2>
 			<div className="w-64 h-52 mt-2">
-				<label for="toggleB" class="flex items-center cursor-pointer select-none mb-2">
+				<label
+					htmlFor="toggleB"
+					className="flex items-center cursor-pointer select-none mb-2"
+				>
 					Enabled
-					<div class="ml-2 relative">
+					<div className="ml-2 relative">
 						<input
 							type="checkbox"
 							id="toggleB"
-							class="sr-only"
+							className="sr-only"
 							checked={oldSettings.humidity.enabled}
-							onChange={(e) => handleEditSettings("humidity", "enabled", e.target.checked ? 1 : 0)}
+							onChange={(e) =>
+								handleEditSettings(
+									"humidity",
+									"enabled",
+									e.target.checked ? 1 : 0
+								)
+							}
 						/>
-						<div class="block bg-gray-600 w-14 h-6 rounded-full"></div>
-						<div class="dot absolute left-1 top-1 bg-red-400 w-4 h-4 rounded-full transition"></div>
+						<div className="block bg-gray-600 w-14 h-6 rounded-full"></div>
+						<div className="dot absolute left-1 top-1 bg-red-400 w-4 h-4 rounded-full transition"></div>
 					</div>
-					<div class="ml-3 text-gray-700 font-medium">
+					<div className="ml-3 text-gray-700 font-medium">
 						{oldSettings.humidity.enabled ? "On" : "Off"}
 					</div>
 				</label>
 				<div className="w-full">
-					<label for="numberPErcentage" className="inline-block">
+					<label htmlFor="numberPErcentage" className="inline-block">
 						Percentage
 					</label>
 					<input
@@ -35,10 +48,17 @@ function HumiditySettings({ oldSettings, handleEditSettings }) {
 						className=" rounded-full border-[1px] border-gray-400 px-3 inline-block w-auto"
 						defaultValue={oldSettings.humidity.humidity}
 						id="numberPErcentage"
-						onChange={(e) => handleEditSettings("humidity", "humidity", Number(e.target.value))}
+						onChange={(e) =>
+							handleEditSettings(
+								"humidity",
+								"humidity",
+								Number(e.target.value)
+							)
+						}
 					/>
 					<h2 className="mt-2 text-xs text-gray-400">
-						The plant will be watered when the humidity is below this percentage.
+						The plant will be watered when the humidity is below
+						this percentage.
 					</h2>
 				</div>
 			</div>
